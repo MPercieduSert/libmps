8fee1202bfdedaeeedc5eaff03fce3935659edb7
Maxime Percie du Sert <maxime.pserciedusert@sfr.fr>
2020-05-31 07:23:50 +0200

    Création de flag

--- entities/typemps.h
+++ entities/typemps.h
@@ -10,6 +10,79 @@ namespace typeMPS {
 using idt = unsigned;
 using codeType = unsigned;
 using szt = unsigned;
+
+class flag {
+public:
+    //! Type de la valuer du drapeau.
+    using flag_type = unsigned;
+protected:
+    flag_type m_value = 0;        //! Entier codant 32 booléen.
+public:
+    //! Constructeur par defaut.
+    flag() = default;
+
+    //! Constructeur.
+    flag(flag_type value) noexcept : m_value(value) {}
+
+    //! Teste si la valeur est nulle.
+    bool operator !() const noexcept
+        {return !m_value;}
+
+    //! Revoie un drapeaux correspondant à l'intersection.
+    flag operator & (flag_type value) const noexcept
+        {return m_value & value;}
+
+    //! Revoie un drapeaux correspondant à l'union.
+    flag operator | (flag_type value) const noexcept
+        {return m_value | value;}
+
+    //! Revoie un drapeaux correspondant à un ou exclusif.
+    flag operator ^ (flag_type value) const noexcept
+        {return m_value ^ value;}
+
+    //! Revoie un drapeaux correspondant à un ou exclusif.
+    flag operator ~ () const noexcept
+        {return ~m_value;}
+
+    //! Revoie un drapeaux correspondant à l'intersection.
+    flag & operator &= (flag_type value) noexcept {
+        m_value &= value;
+        return *this;}
+
+    //! Revoie un drapeaux correspondant à l'union.
+    flag & operator |= (flag_type value) noexcept {
+        m_value |= value;
+        return *this;
+    }
+
+    //! Revoie un drapeaux correspondant à un ou exclusif.
+    flag operator ^= (flag_type value) noexcept {
+        m_value ^= value;
+        return *this;
+    }
+
+    //! Conversion.
+    operator flag_type() const noexcept
+        {return m_value;}
+
+    //! Conversion.
+    flag & operator =(flag_type value) noexcept {
+        m_value = value;
+        return *this;
+    }
+
+    //! Mutateur de la valeur.
+    void setValue(flag_type value) noexcept
+        {m_value = value;}
+
+    //! Teste si l'intersection n'est pas vide.
+    bool test(flag_type value) const noexcept
+        {return m_value & value;}
+
+    //! Accesseur de la valeur.
+    flag_type value() const noexcept
+        {return m_value;}
+};
 }
 
 #endif // TYPEMPS_H
